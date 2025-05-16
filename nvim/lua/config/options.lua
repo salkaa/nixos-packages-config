445365ab24ca676ed4c2bcfc0edf722963a720d1
salkaa <matus.palka@gmail.com>
2025-05-17 00:56:26 +0200

    kill spellcheck

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -6,4 +6,4 @@ vim.opt.tabstop = 4        -- Number of spaces a tab counts for
 vim.opt.shiftwidth = 4     -- Number of spaces for each step of (auto)indent
 vim.opt.softtabstop = 4    -- Number of spaces a tab counts for while editing
 vim.opt.expandtab = true   -- Convert tabs to spaces
-
+vim.opt.spell = false
